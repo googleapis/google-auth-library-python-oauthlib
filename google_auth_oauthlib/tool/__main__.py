d670fb8dffe733ce86f95a8492de23c1f25fa464
Jon Wayne Parrott <jonwayne@google.com>
2017-05-11 13:51:08 -0700

    Remove unused import

--- google_auth_oauthlib/tool/__main__.py
+++ google_auth_oauthlib/tool/__main__.py
@@ -30,7 +30,6 @@ authorization flow to get authorization from the users.
 .. _section 1.3.1 of RFC6749: https://tools.ietf.org/html/rfc6749#section-1.3.1
 """
 
-import io
 import json
 import os
 import os.path
