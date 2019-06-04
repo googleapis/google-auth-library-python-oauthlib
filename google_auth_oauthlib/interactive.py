8a960280edd7d5780df90f6293881b3a58904162
Tim Swast <swast@google.com>
2019-06-04 15:59:52 -0700

    Add example for using `get_user_credentials`.

--- google_auth_oauthlib/interactive.py
+++ google_auth_oauthlib/interactive.py
@@ -57,6 +57,35 @@ def get_user_credentials(scopes, client_id, client_secret):
     Returns:
         google.oauth2.credentials.Credentials:
             The OAuth 2.0 credentials for the user.
+
+    Examples:
+        Get credentials for your user account and use them to run a query
+        with BigQuery::
+
+            import google_auth_oauthlib
+
+            # TODO: Create a client ID for your project.
+            client_id = "YOUR-CLIENT-ID.apps.googleusercontent.com"
+            client_secret = "abc_ThIsIsAsEcReT"
+
+            # TODO: Choose the needed scopes for your applications.
+            scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+
+            credentials = google_auth_oauthlib.get_user_credentials(
+                scopes, client_id, client_secret
+            )
+
+            # 1. Open the link.
+            # 2. Authorize the application to have access to your account.
+            # 3. Copy and paste the authorization code to the prompt.
+
+            # Use the credentials to construct a client for Google APIs.
+            from google.cloud import bigquery
+
+            bigquery_client = bigquery.Client(
+                credentials=credentials, project="your-project-id"
+            )
+            print(list(bigquery_client.query("SELECT 1").result()))
     """
 
     client_config = {
