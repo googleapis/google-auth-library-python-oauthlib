d90af6c49a8045c194187a4d312c29ec56d9fe22
Bu Sun Kim <busunkim@google.com>
2020-10-28 00:34:30 +0000

    docs: add 3.9 classifier

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
