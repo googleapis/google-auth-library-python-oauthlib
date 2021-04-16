f41f1e1d3fa4a1a25cd2a962bac2a04c0e8390f2
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-16 02:22:49 -0400

    chore: update workaround to make sic work

--- setup.py
+++ setup.py
@@ -16,6 +16,21 @@ import io
 
 import setuptools
 
+# Disable version normalization performed by setuptools.setup()
+try:
+    # Try the approach of using sic(), added in setuptools 46.1.0
+    from setuptools import sic
+except ImportError:
+    # Try the approach of replacing packaging.version.Version
+    sic = lambda v: v
+    try:
+        # setuptools >=39.0.0 uses packaging from setuptools.extern
+        from setuptools.extern import packaging
+    except ImportError:
+        # setuptools <39.0.0 uses packaging from pkg_resources.extern
+        from pkg_resources.extern import packaging
+    packaging.version.Version = packaging.version.LegacyVersion
+
 TOOL_DEPENDENCIES = "click>=6.0.0"
 
 DEPENDENCIES = ("google-auth>=1.0.0", "requests-oauthlib>=0.7.0")
@@ -29,7 +44,7 @@ version = "0.4.4"
 
 setuptools.setup(
     name="google-auth-oauthlib",
-    version=setuptools.sic(version),
+    version=sic(version),
     author="Google Cloud Platform",
     author_email="jonwayne+google-auth@google.com",
     description="Google Authentication Library",
